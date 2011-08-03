e0fce7b1a79541a1fcf133f0b1a46a44e5d212bb
cgnoerlich <>
2011-08-03 19:16:30 +0000

    0.79 devel-4

---
+++ dvdisaster/documentation/include/dict_cs.php
@@ -0,0 +1,30 @@
+<?php
+# dvdisaster: English homepage dictionary
+# Copyright (C) 2004-2011 Carsten Gnörlich
+#
+# UTF-8 trigger: äöüß 
+
+# English dictionary for some phrases used in dvdisaster.php
+
+$trans_version="verze";
+
+$trans_to_internet="Na Internetovou verzi";
+$trans_to_hoster="Přehled (na SourceForge.net)";
+
+$trans_back="Zpět";
+$trans_screen_shot="Screen shot";
+
+$trans_contents="Obsah";
+$trans_news="News";
+$trans_atom_title="dvdisaster news";
+$trans_hosting="Hostováno pod";
+
+$trans_fdl="Doslovné kopírování a distribuce celého tohoto článku jsou pro jakékoli médium povoleny za předpokladu, že tato poznámka zůstane zachována.";
+$trans_copyright="Autorská práva 2004-2011 Carsten Gnörlich, k překladu 2006 Luboš Staněk.";
+
+$trans_sourcecode="Source code for all operating systems:";
+$trans_signature="Digital signature:";
+$trans_md5="MD5 checksum:";
+$trans_macbinary="Binary for Mac OS X 10.5 / x86:";
+$trans_winbinary="Binary for Windows:";
+?>
